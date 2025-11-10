9e9575d9750601c25ab7d2307fd9501098fef05b
hcdbp24c3 <hcdbp24c3@proton.me>
2025-11-10 07:44:29 +0700

    up

--- js/index.js
+++ js/index.js
@@ -1,26 +1,3 @@
-function toggleDarkMode(){
-    var body = document.body;
-    body.classList.toggle("dark-mode");
-
-    // Cập nhật text nút
-    const btn = document.getElementById("darkModeToggle");
-    if(body.classList.contains("dark-mode")){
-        btn.textContent = "Tắt chế độ ban đêm";
-    } else {
-        btn.textContent = "Bật chế độ ban đêm";
-    }
+function test {
+    document.getElementById=("title").innerHTML = "Xin chào";
 }
-
-// Gán sự kiện nút bật/tắt chế độ ban đêm
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("darkModeToggle").addEventListener("click", toggleDarkMode);
-
-    document.getElementById("contact-form").addEventListener("submit", function(event) {
-        const emailInput = document.getElementById("email");
-        if (!emailInput.value) {
-            event.preventDefault();
-            alert("Vui lòng nhập email");
-            emailInput.focus();
-        }
-    });
-});
