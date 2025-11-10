c761b1bdb740544fd27a24e77b1dc7fdced9b710
hcdbp24c3 <hcdbp24c3@proton.me>
2025-11-10 08:25:17 +0700

    update contact.html, index.html and js/index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-function darkmode() {
-    var body = document.body;
-    body.classList.toggle("dark-mode");
+function checkform() {
+    const c = document.getElementById("Email");
+    if (!c.value) {
+        alert("Vui lòng nhập email!");
+        return false;
+    }
+    return true;
 }
